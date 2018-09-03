7f53130c0bc315d775dcc6a11cc0449dbb5fd163
Erick Sanchez <erick.sanchez.p05@gmail.com>
2018-09-03 02:00:44 -0500

    Added styles and scripts to be register and enqued

--- external/functions.js
+++ external/functions.js
@@ -6,12 +6,32 @@
 
 function Functions(server) {
 
+	console.log("Functiones inited");
+
 	// Adding core classes
 	this.authentication = require('../framework/authentication.js');
 	this.tokenizr = require('../framework/tokenizr.js');
+	var dipper = global.dipper;
+
+	// Adding styles
+	//dipper.registerStyle('magnific-popup', '//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css');
+	dipper.enqueueStyle('magnific-popup');
+
+	// Adding scripts
 
 	// Adding endpoints
 	EndpointApp = require('./endpoint/app.endpoint.js');
+
+	// Adding meta tags
+	// Meta tags
+	dipper.addMeta('UTF-8', '', 'charset');
+	dipper.addMeta('viewport', 'width=device-width, initial-scale=1');
+	dipper.addMeta('og:title', dipper.getPageTitle(), 'property');
+	dipper.addMeta('og:site_name', dipper.getSiteTitle(), 'property');
+	dipper.addMeta('og:description', dipper.getDescription(), 'property');
+	//dipper.addMeta('og:image', dipper.urlTo('/favicon.png'), 'property');
+	dipper.addMeta('og:type', 'website', 'property');
+	//dipper.addMeta('og:url', $site->urlTo('/'), 'property');
 
 	// Adding models
 	// --
@@ -21,19 +41,6 @@ function Functions(server) {
 	this.endpoints['EndpointApp'] = new EndpointApp(server);
 
 	this.models = [];
-
-	// Meta tags
-	var dipper = global.dipper;
-
-	dipper.addMeta('UTF-8', '', 'charset');
-	dipper.addMeta('viewport', 'width=device-width, initial-scale=1');
-	dipper.addMeta('og:title', dipper.getPageTitle(), 'property');
-	dipper.addMeta('og:site_name', dipper.getSiteTitle(), 'property');
-	dipper.addMeta('og:description', dipper.getDescription(), 'property');
-	//dipper.addMeta('og:image', dipper.urlTo('/favicon.png'), 'property');
-	dipper.addMeta('og:type', 'website', 'property');
-	//dipper.addMeta('og:url', $site->urlTo('/'), 'property');
-	dipper.metaTags();
 
 }
 
